82727a65a5adf40f95e60c3676cf91865b0a0842
hcw5528636 <798094023@qq.com>
2019-08-20 20:20:59 +0800

    123

---
+++ src/Utils2Type.scala
@@ -0,0 +1,18 @@
+package com.Utils
+
+object Utils2Type {
+  //String 转 Int
+  def toInt(str:String): Int ={
+
+    str.toInt
+
+  }
+
+  //String 转 Double
+  def toDouble(str:String): Double ={
+
+    str.toDouble
+
+  }
+
+}
