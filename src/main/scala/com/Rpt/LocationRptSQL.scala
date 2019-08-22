ee9d46d81870cb4b6b401a8a005209ecdc925f3f
hcw5528636 <798094023@qq.com>
2019-08-22 10:32:23 +0800

    51

--- src/main/scala/com/Rpt/LocationRptSQL.scala
+++ src/main/scala/com/Rpt/LocationRptSQL.scala
@@ -1,34 +1,33 @@
 package com.Rpt
 
-import org.apache.spark.SparkConf
-import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.sql.{DataFrame, SQLContext, SparkSession}
 
 object LocationRptSQL {
   def main (args: Array[String]): Unit = {
     // 判断路径
-    if(args.length != 1){
-      println("目录错误！！")
-      sys.exit()
-    }
+    //    if(args.length != 1){
+//      println("目录错误！！")
+//      sys.exit()
+//    }
     // 创建输入输出路径
+
     val Array(inputPath) = args
-
-    val conf = new SparkConf()
-      .setAppName(this.getClass.getName)
-      .setMaster("local[*]")
+    val conf = new SparkConf().setAppName(this.getClass.getName).setMaster("local[*]")
+      // 设置序列化方式 采用Kyro序列化方式，比默认序列化方式性能高
       .set("spark.serializer","org.apache.spark.serializer.KryoSerializer")
-
     // 创建执行入口
-   val spark =  SparkSession
-     .builder().config("spark.sql.parquet.compression.codec","snappy")
-     .config(conf)
-     .getOrCreate()
+    val sc = new SparkContext(conf)
+    val sQLContext = new SQLContext(sc)
+    // 设置压缩方式 使用Snappy方式进行压缩
+    sQLContext.setConf("spark.sql.parquet.compression.codec","snappy")
+    // 读取本地文件
+    val df = sQLContext.read.parquet(inputPath)
 
-    val df: DataFrame = spark.read.parquet(inputPath)
 
-    df.createOrReplaceTempView("log")
+    df.registerTempTable("log")
       // 生成临时表
-    val res = spark.sql ("select provincename,cityname," +
+      val result = sQLContext.sql("select provincename,cityname," +
       "sum(case when requestmode = 1 and processnode >= 1 then 1 else 0 end) ysSum," +
       "sum(case when requestmode = 1 and processnode >= 2 then 1 else 0 end) yxSum," +
       "sum(case when requestmode = 1 and processnode = 3 then 1 else 0 end) adSum," +
@@ -41,8 +40,8 @@ object LocationRptSQL {
       "from log group by provincename,cityname")
 
 
-    res.show()
+    result.coalesce(1).write.partitionBy("provincename","cityname").json(outputPath)
 
-    spark.stop()
+    sc.stop()
   }
 }
